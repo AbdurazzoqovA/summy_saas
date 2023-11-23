f6d8955a45606addf95ac9af419026376e74e84e
Muhammadayub Baxtiyorov <bakhtiyorovma@gmail.com>
2023-11-23 23:08:08 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "laptopsmate@gmail.com"
-EMAIL_HOST_PASSWORD = "rabynmrcxnqjkulw"
+EMAIL_HOST_USER = "detectingai.com@gmail.com"
+EMAIL_HOST_PASSWORD = "tbsamlvxcboptpzq"
 
 # LOGIN_URL = "login"
 
