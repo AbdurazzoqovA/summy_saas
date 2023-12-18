5f6a1cfab742cf0a540832406798221d49bbf71b
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2023-12-18 17:39:20 +0500

    update

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . models import Subscription, WordCountTracker
 # Register your models here.
+admin.site.register(Subscription)
+admin.site.register(WordCountTracker)
