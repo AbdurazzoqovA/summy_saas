311a6650667f71ef62d1d8a3dbdf5339683575f5
Omer Bhatti <omer.sharif@arbisoft.com>
2024-01-06 21:13:54 +0500

    update

--- static/front/send_request.js
+++ static/front/send_request.js
@@ -1,54 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const summaryButton = document.getElementById('summaryButton');
-    const neighbourPanel = document.querySelector('.neighbourPanel');
-    const resizablePanel = document.querySelector('.resizablePanel');
-    const spinner = document.getElementById('spinner');
-
+document.addEventListener("DOMContentLoaded", function () {
+    const summaryButton = document.getElementById("summaryButton");
+    const neighbourPanel = document.querySelector(".neighbourPanel");
+    const resizablePanel = document.querySelector(".resizablePanel");
+    const spinner = document.getElementById("spinner");
+  
     // Function to get CSRF token from the meta tag
     function getCsrfToken() {
-        return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+      return document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content");
     }
-
-    summaryButton.addEventListener('click', function() {
-        const text = neighbourPanel.textContent || neighbourPanel.innerText;
-        const data = { text: text };
-        console.log("button is pressed")
-        // Show spinner and disable button
-        spinner.style.display = 'block';
-        summaryButton.disabled = true;
-        let alertError = document.getElementById('alertError');
-        
-        // Send POST request
-        fetch('https://summarygenerator.io/en/summary/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCsrfToken(),
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                // If the response includes an error key, display it
-                alertError.textContent = data.error; // Set the error message
-                alertError.style.display = 'block'; // Show the alert
-            } else {
-                // If the response is successful, update the resizablePanel
-                alertError.style.display = 'none';
-                resizablePanel.textContent = data.summary; // Assuming the response has a "summary" key
-            }
-            // Update the resizablePanel with the summary
+  
+    summaryButton.addEventListener("click", function () {
+      const text = neighbourPanel.textContent || neighbourPanel.innerText;
+      const data = { text: text };
+      console.log("button is pressed");
+      // Show spinner and disable button
+      spinner.style.display = "block";
+      summaryButton.disabled = true;
+      let alertError = document.getElementById("alertError");
+  
+      // Send POST request
+      fetch("https://summarygenerator.io/en/summary/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": getCsrfToken(),
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            // If the response includes an error key, display it
+            alertError.textContent = data.error; // Set the error message
+            alertError.style.display = "block"; // Show the alert
+          } else {
+            // If the response is successful, update the resizablePanel
+            alertError.style.display = "none";
             resizablePanel.textContent = data.summary; // Assuming the response has a "summary" key
+          }
+          // Update the resizablePanel with the summary
+          resizablePanel.textContent = data.summary; // Assuming the response has a "summary" key
+          const wordCountElement = document.getElementById("word-count-summary");
+          const words = data.summary
+              .trim()
+              .split(/\s+/)
+              .filter((w) => w);
+          wordCountElement.innerText = `Words: ${words.length}`;
         })
         .catch((error) => {
-            
-            console.error('Error:', error);
+          console.error("Error:", error);
         })
         .finally(() => {
-            // Hide spinner and enable button
-            spinner.style.display = 'none';
-            summaryButton.disabled = false;
+          // Hide spinner and enable button
+          spinner.style.display = "none";
+          summaryButton.disabled = false;
         });
     });
-});
+  });
+  
