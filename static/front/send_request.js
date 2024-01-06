f9df6248ed0f61a34a72db604b9b766fe163314c
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2024-01-06 15:44:41 +0500

    update

--- static/front/send_request.js
+++ static/front/send_request.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let alertError = document.getElementById('alertError');
         
         // Send POST request
-        fetch('/summary/', {
+        fetch('https://summarygenerator.io/summary/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
