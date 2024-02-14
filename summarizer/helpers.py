5e9a483be50582ea570a23dd26de0efe72bf0423
raheel185 <nabeelfida185@gmail.com>
2024-02-14 19:30:20 +0500

    change

--- summarizer/helpers.py
+++ summarizer/helpers.py
@@ -39,7 +39,7 @@ def summarizer(
     #  Summarize the text and shorten it by {range_percentage}, text:
     if mode == "Paragraph":
         prompt = f"""
-       Summarize the following text to approximately 250 words:
+       Summarize the following text to approximately 265 words:
         {text}
 
         """
