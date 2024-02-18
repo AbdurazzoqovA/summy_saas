80291f3b96c9a2b5759d301eb1fe5611419bbd90
raheel185 <nabeelfida185@gmail.com>
2024-02-18 19:07:10 +0500

    change

--- static/front/send_request.js
+++ static/front/send_request.js
@@ -7,33 +7,12 @@ function decodeHtml(html) {
   return html.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
 }
 
-function converToBullets(text) {
-  const resizablePanel = document.querySelector(".resizablePanel");
-  var textContent = text.trim();
-  var lines = textContent.split("-");
-
-  // Create a new ul element
-  var ulElement = document.createElement("ul");
-  ulElement.className = "paraList";
-
-  // Iterate through each line and create a li element for each
-  lines.forEach(function (line) {
-    var liElement = document.createElement("li");
-    liElement.textContent = line.trim(); // Exclude the leading '-'
-    if (line.length > 4) ulElement.appendChild(liElement);
-  });
-
-  resizablePanel.innerHTML = "";
-
-  resizablePanel.appendChild(ulElement);
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const summaryButton = document.getElementById("summaryButton");
   const neighbourPanel = document.querySelector(".neighbourPanel");
   const resizablePanel = document.querySelector(".resizablePanel");
   const spinner = document.getElementById("summary-spinner");
-  console.log("21");
+
   // Function to get CSRF token from the meta tag
   function getCsrfToken() {
     return document
@@ -41,7 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
       .getAttribute("content");
   }
 
-  function requestforSummary(text) {
+  summaryButton.addEventListener("click", function () {
+    const text = neighbourPanel.textContent || neighbourPanel.innerText;
     // get button which has selected attribute
     let mode = document.querySelector(".mode-button[selected]").innerText;
     let rangeValue = 0;
@@ -49,13 +29,11 @@ document.addEventListener("DOMContentLoaded", function () {
       rangeValue = document.getElementById("summary-length").value;
     }
     const data = { text: text, mode: mode, rangeValue: rangeValue };
-
-    console.log(mode);
     // Show spinner and disable button
     spinner.style.display = "block";
     summaryButton.disabled = true;
     let alertError = document.getElementById("alertError");
-    console.log("Data - ", data);
+
     // Send POST request
     fetch("https://summarygenerator.io/en/summary/", {
       method: "POST",
@@ -67,8 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(mode);
-
         if (data.error) {
           // If the response includes an error key, display it
           alertError.innerHTML = data.error; // Set the error message
@@ -77,8 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
           // If the response is successful, update the resizablePanel
           alertError.style.display = "none";
           // let result = marked.marked(data.summary);
-          if (mode === "Paragraph") resizablePanel.innerHTML = data?.summary;
-          else converToBullets(data.summary);
+          resizablePanel.innerHTML = data?.summary; // Assuming the response has a "summary" key
         }
         const wordCountElements = document.querySelectorAll(
           "#word-count-summary"
@@ -99,22 +74,5 @@ document.addEventListener("DOMContentLoaded", function () {
         spinner.style.display = "none";
         summaryButton.disabled = false;
       });
-  }
-
-  summaryButton.addEventListener("click", function () {
-    const text = neighbourPanel.textContent || neighbourPanel.innerText;
-
-    var wordCount = text.split(/\s+/).filter(function (word) {
-      return word.length > 0;
-    }).length;
-    //console.log("Counting-Words", wordCount);
-    console.log("Text", text);
-    var wordLimit = 800;
-
-    if (wordCount <= wordLimit) requestforSummary(text);
-    else {
-      // document.getElementById("upgradePopup").style.display = "block";
-      alert("Word Limit Exceeded! Upgrade to Pro for more features");
-    }
   });
 });
