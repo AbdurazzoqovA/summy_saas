ed63c2173c6636bdf5b8237b2cf7dc08dd3d0a92
raheel185 <nabeelfida185@gmail.com>
2024-02-29 13:22:47 +0500

    change

--- summarizer/urls.py
+++ summarizer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls.i18n import i18n_patterns
 from . import views
 
 urlpatterns = [
+    path('i18n/', i18n_patterns),
     path("home2/", views.main, name="home2"),
     path("summary/", views.summary),
     path("blog/", views.blog, name="blog"),
@@ -27,6 +29,7 @@ urlpatterns = [
     path("terms/", views.terms, name="terms"),
     path("privacy/", views.privacy, name="privacy"),
     path("contact/", views.contact, name="contact"),
+    
 ]
 
 
