014345340194dfbffb2d51ff5962b76d4a2c1275
Kamran Ali <kamran@192.168.0.104>
2024-03-14 19:44:21 +0500

    changes in helpers

--- summarizer/helpers.py
+++ summarizer/helpers.py
@@ -5,7 +5,6 @@ import openai
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 # Load environment variables from .env file
@@ -15,25 +14,24 @@ palm.configure(api_key="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")
 
 
 def summarizer(
-    text,
-    mode,
-    range_value,
+        text,
+        mode,
+        range_value,
 ):
-
     # summary_length_tokens = 100
     word_count = len(text.split())
 
-    summaryLen = int(word_count*0.9)
+    summaryLen = int(word_count * 0.9)
 
     model = "models/text-bison-001"
     range_value = int(range_value)
 
     if range_value == 3:
-        summaryLen = int(word_count/3)
+        summaryLen = int(word_count * 0.77)
     elif range_value == 2:
-        summaryLen = int(word_count/2)
+        summaryLen = int(word_count * 0.45)
     elif range_value == 1:
-        summaryLen = int(word_count/1.5)
+        summaryLen = int(word_count * 0.30)
 
     #  Summarize the text and shorten it by {range_percentage}, text:
     if mode == "Paragraph":
@@ -44,7 +42,7 @@ def summarizer(
         """
     else:
         prompt = f"""
-       summarize the text and present it in bullet points important, Also format the output text nicely, use headings. text:
+       summarize the text and present it in bullet points important, text:
         {text}
         """
 
@@ -52,19 +50,19 @@ def summarizer(
         openai_api_key = os.environ.get("OPEN_AI_KEY")
         client = openai.OpenAI(api_key=openai_api_key)
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",  # Experiment with different models as needed
+            model="gpt-3.5-turbo-0125",  # Experiment with different models as needed
             messages=[
                 {
                     "role": "system",
-                    "content": f"you are expert to summarize texts, summarize given text and return only markdown result no extra.",
+                    "content": f"you are expert to summarize text.",
                 },
                 {
                     "role": "user",
                     "content": prompt,
                 },
             ],
-            max_tokens=4000,
-            temperature=1,
+            max_tokens=int(word_count),
+            temperature=0.9,
         )
 
         return response.choices[0].message.content
